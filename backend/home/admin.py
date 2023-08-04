beacd62676627942f0f61f6ec2b4c9d8c1639c70
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-04 15:41:25 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ebank
+admin.site.register(Ebank)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Ebank(models.Model):
+    'Generated Model'
+    address = models.CharField(max_length=255,)
+    provides_card = models.BooleanField()
+    name = models.CharField(max_length=255,)
